f7b36f0087ee230229edcf59ab47b2b72a3489f2
maulidacy <cahyamaulida011@gmail.com>
2025-07-27 19:54:59 +0700

    first commit

--- Kasir Sederhana CLI/Kasir_Sederhana_CLI.py
+++ Kasir Sederhana CLI/Kasir_Sederhana_CLI.py
@@ -1,15 +1,35 @@
-nama_barang = input("Masukkan nama barang: ")
-harga_barang = float(input("Masukkan harga barang: "))
-jumlah_barang = int(input("Masukkan jumlah barang: "))
+# Kasir Sederhana CLI
+# Program ini memungkinkan pengguna untuk menambahkan barang ke total belanja dan menghitung total belanja setelah diskon
+print("Selamat datang di Kasir Sederhana CLI!")
 
-total_belanja = harga_barang * jumlah_barang
+barang_yang_dibeli = []
+total_belanja = 0
 diskon = 0
 total_setelah_diskon = 0
 
-if total_belanja > 100.000:
-    print("Selamat anda mendapatkan diskon 20%")
-    diskon = total_belanja * 0.20
-    total_setelah_diskon = total_belanja - diskon
-    print("Total Belanja: ", total_setelah_diskon)
-else:
-    print("Total Belanja: ", total_belanja)
+while True:
+    nama_barang = input("Masukkan nama barang: ")
+    harga_barang = float(input("Masukkan harga barang: "))
+    jumlah_barang = int(input("Masukkan jumlah barang: "))
+    tambah_barang = input("Tambahkan barang lagi? (Y/N): ")
+    
+    def diskon():
+         if total_belanja > 100000:
+            diskon = total_belanja * 0.20
+            print("Selamat anda mendapatkan diskon 20%")
+            total_setelah_diskon = total_belanja - diskon
+            print("Total Belanja: ", total_setelah_diskon)
+            exit()
+         else: 
+            print("Total Belanja: ", total_belanja)
+            exit()
+
+    if tambah_barang == 'N':
+        total_belanja += harga_barang * jumlah_barang
+        diskon()  # Panggil fungsi diskon
+    elif tambah_barang == 'Y':
+        total_belanja += harga_barang * jumlah_barang
+        diskon()  # Panggil fungsi diskon
+    else:
+        print("Pilihan tidak valid, silakan coba lagi.")
+        
