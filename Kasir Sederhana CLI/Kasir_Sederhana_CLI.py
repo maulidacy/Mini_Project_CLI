e8e0b3897df07d42c21c3309f99b62ddd0fb6739
maulidacy <cahyamaulida011@gmail.com>
2025-07-27 22:23:41 +0700

    first commit

--- Kasir Sederhana CLI/Kasir_Sederhana_CLI.py
+++ Kasir Sederhana CLI/Kasir_Sederhana_CLI.py
@@ -40,7 +40,7 @@ def tampilkan_struk(barang_yang_dibeli, total_belanja):
     print("Total Setelah Diskon: ", total_setelah_diskon)
     print("Barang yang dibeli:")
     for barang in barang_yang_dibeli:
-        print(f"- {barang[0]} x {barang[2]} = {barang[1] * barang[2]}")
+        print(f"- {barang[0]} x {barang[2]} = {barang[1] * barang[2]}") #looping untuk menampilkan daftar barang yang sudah dibeli
     print("Terima kasih telah berbelanja di Kasir Sederhana CLI!")
     print("=====================================================")
 
